0e480c58eaff0a0886628f06aa4870a4b97a767f
Mario Constanti <mario.constanti@mercedes-benz.com>
2023-08-16 08:03:14 +0200

    bugfix: define the mockgen binary path
    goreleaser runs go generate during the release process and with that,
    the mockgen binary must exist in PATH or the path is explicitly defined

--- pkg/client/mock/doc.go
+++ pkg/client/mock/doc.go
@@ -1,5 +1,9 @@
 package mock
 
-//go:generate mockgen -package mock -destination=enterprise.go -source=../enterprise.go Enterprise
-//go:generate mockgen -package mock -destination=organization.go -source=../organization.go Organization
-//go:generate mockgen -package mock -destination=pool.go -source=../pool.go Pool
+// We use the mockgen binary which is installed via make mockgen.
+// This helps us to get consistent builds and also make the release process work
+// without chaning PATH variables etc.
+
+//go:generate ../../../bin/mockgen -package mock -destination=enterprise.go -source=../enterprise.go Enterprise
+//go:generate ../../../bin/mockgen -package mock -destination=organization.go -source=../organization.go Organization
+//go:generate ../../../bin/mockgen -package mock -destination=pool.go -source=../pool.go Pool
