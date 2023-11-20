c202322aea47a6c562019630c3fdea899c4ba05f
Heiko Kast <heiko.kast@mercedes-benz.com>
2023-11-20 14:32:24 +0100

    feat: add defaults package

---
+++ pkg/defaults/defaults.go
@@ -0,0 +1,13 @@
+package defaults
+
+import "time"
+
+const (
+	// default values for operator configuration
+	DefaultMetricsBindAddress     = ":8080"
+	DefaultHealthProbeBindAddress = ":8081"
+	DefaultLeaderElect            = false
+	DefaultSyncPeriod             = 5 * time.Minute
+	DefaultNamespace              = ""
+	DefaultCreateWebhook          = false
+)
