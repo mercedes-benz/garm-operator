11b2477d9fe4cfc3942c91806d7986a9c4599d2b
rthalho <rafael.thalhofer@mercedes-benz.com>
2024-01-16 09:13:21 +0100

    fix: remove unnecessary log messages, handle runner cr not found error

--- internal/controller/runner_controller.go
+++ internal/controller/runner_controller.go
@@ -45,11 +45,7 @@ type RunnerReconciler struct {
 //+kubebuilder:rbac:groups=garm-operator.mercedes-benz.com,namespace=xxxxx,resources=runners/finalizers,verbs=update
 
 func (r *RunnerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := log.FromContext(ctx)
-	log.Info("Reconciling runners...", "Request", req)
-
 	instanceClient := garmClient.NewInstanceClient()
-
 	return r.reconcile(ctx, req, instanceClient)
 }
 
@@ -76,9 +72,16 @@ func (r *RunnerReconciler) reconcile(ctx context.Context, req ctrl.Request, inst
 }
 
 func (r *RunnerReconciler) handleCreateRunnerCR(ctx context.Context, req ctrl.Request, fetchErr error, garmRunner *params.Instance) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
 	if apierrors.IsNotFound(fetchErr) && garmRunner != nil {
 		return r.createRunnerCR(ctx, garmRunner, req.Namespace)
 	}
+
+	if apierrors.IsNotFound(fetchErr) {
+		log.Info("object was not found")
+		return ctrl.Result{}, nil
+	}
+
 	return ctrl.Result{}, fetchErr
 }
 
@@ -213,9 +216,7 @@ func (r *RunnerReconciler) PollRunnerInstances(ctx context.Context, eventChan ch
 			close(eventChan)
 			return
 		case <-ticker.C:
-			log.Info("Polling Runners...")
 			instanceClient := garmClient.NewInstanceClient()
-
 			err := r.EnqueueRunnerInstances(ctx, instanceClient, eventChan)
 			if err != nil {
 				log.Error(err, "Failed polling runner instances")
