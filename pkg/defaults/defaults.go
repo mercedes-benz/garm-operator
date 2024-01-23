18dea4701241deac3161f202ac92d2e4be4f1915
Mario Constanti <mario.constanti@mercedes-benz.com>
2024-01-23 11:29:00 +0100

    fix: set default to 2 hours
    Signed-off-by: Mario Constanti <mario.constanti@mercedes-benz.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -12,7 +12,7 @@ const (
 	DefaultSyncPeriod             = 5 * time.Minute
 	DefaultWatchNamespace         = ""
 	DefaultSyncRunnersInterval    = 5 * time.Second
-	DefaultMinIdleRunnersAge      = 10 * time.Minute
+	DefaultMinIdleRunnersAge      = 2 * time.Hour
 
 	// default values for garm configuration
 	DefaultGarmInit  = true
