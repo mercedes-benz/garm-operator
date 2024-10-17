528e8252c60061e2237b06de24dc8027d85cb036
Mario Constanti <mario.constanti@mercedes-benz.com>
2024-10-17 07:34:45 +0200

    fix: remove unused logger

--- api/v1alpha1/enterprise_webhook.go
+++ api/v1alpha1/enterprise_webhook.go
@@ -4,11 +4,7 @@ package v1alpha1
 
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
-
-// log is for logging in this package.
-var enterpriselog = logf.Log.WithName("enterprise-resource")
 
 func (r *Enterprise) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
--- api/v1alpha1/organization_webhook.go
+++ api/v1alpha1/organization_webhook.go
@@ -4,11 +4,7 @@ package v1alpha1
 
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
-
-// log is for logging in this package.
-var organizationlog = logf.Log.WithName("organization-resource")
 
 func (r *Organization) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
