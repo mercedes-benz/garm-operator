ea408ac1d528ba4f7471deef84ce94016c2a78c8
Mario Constanti <mario.constanti@mercedes-benz.com>
2024-11-19 17:04:23 +0100

    fix: apply a dummy default controller config
    to make garm work after the initialization, we apply a default config
    for webhookURL, metadataURL and callbackURL
    
    this config will be overwritten once a garmserverconfig CR got applied
    
    Signed-off-by: Mario Constanti <mario.constanti@mercedes-benz.com>

--- pkg/client/controller.go
+++ pkg/client/controller.go
@@ -3,9 +3,12 @@ package client
 
 import (
 	"github.com/cloudbase/garm/client/controller"
+	garmcontroller "github.com/cloudbase/garm/client/controller"
 	"github.com/cloudbase/garm/client/controller_info"
+	"github.com/cloudbase/garm/params"
 
 	"github.com/mercedes-benz/garm-operator/pkg/metrics"
+	"github.com/mercedes-benz/garm-operator/pkg/util"
 )
 
 type ControllerClient interface {
@@ -23,12 +26,33 @@ func NewControllerClient() ControllerClient {
 	}
 }
 
+const (
+	initialMetadataURL = "https://initial.metadata.garm.local"
+	initialCallbackURL = "https://initial.callback.garm.local"
+	initialWebhookURL  = "https://initial.webhook.garm.local"
+)
+
 func (s *controllerClient) GetControllerInfo() (*controller_info.ControllerInfoOK, error) {
 	return EnsureAuth(func() (*controller_info.ControllerInfoOK, error) {
 		metrics.TotalGarmCalls.WithLabelValues("controller.Info").Inc()
 		controllerInfo, err := s.GarmAPI().ControllerInfo.ControllerInfo(&controller_info.ControllerInfoParams{}, s.Token())
 		if err != nil {
 			metrics.GarmCallErrors.WithLabelValues("controller.Info").Inc()
+
+			// after the first run, garm needs a configuration for webhook, metadata and callback
+			// to make garm work after the first run, we set some defaults
+			if IsConflictError(err) {
+				updateParams := garmcontroller.NewUpdateControllerParams().WithBody(params.UpdateControllerParams{
+					MetadataURL: util.StringPtr(initialMetadataURL),
+					CallbackURL: util.StringPtr(initialCallbackURL),
+					WebhookURL:  util.StringPtr(initialWebhookURL),
+				})
+				// let's initiate the new controller with some defaults
+				_, err := s.UpdateController(updateParams)
+				if err != nil {
+					return nil, err
+				}
+			}
 			return nil, err
 		}
 		return controllerInfo, nil
